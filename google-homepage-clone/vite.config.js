025e40c35fa89134a9b3f9e7ed4bacc11a5ca664
ashajyothi06 <120666428+ashajyothi06@users.noreply.github.com>
2025-06-13 22:39:43 +0530

    add

--- google-homepage-clone/vite.config.js
+++ google-homepage-clone/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/google-homepage-clone/', // 👈 required for GitHub Pages
   plugins: [react()],
 })
